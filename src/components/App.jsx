8c4ffb5365d9cf0bfdc740911e1db206280cb928
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-20 07:06:44 +0100

    base structure added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form action="">
+          <h2>Name</h2>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button>Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li>Rosie Simpson</li>
+          <li>Hermione Kline</li>
+          <li>Eden Clements</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+/* <h1>Phonebook</h1>
+  <ContactForm />
+
+  <h2>Contacts</h2>
+  <Filter />
+  <ContactList />*/
