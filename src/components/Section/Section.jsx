d14c93c0153ada4c6f0ccc571607a09081acdf6e
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-21 20:13:14 +0100

    section added

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -11,10 +11,10 @@ export default class Section extends Component {
   };
   render() {
     return (
-      <div>
-        <h2>{this.props.title}</h2>
-        {this.props.children}
-      </div>
+        <section>
+          <h2>{this.props.title}</h2>
+          {this.props.children}
+        </section>
     );
   }
 }
